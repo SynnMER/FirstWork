ed4e91ea74dd85df5264c5717a311a5add1e2a2f
Artur Ilyasov <Scorpion_run_142@mail.ru>
2024-03-19 10:58:19 +0500

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactory;
+using ConcreteFactory;
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPosudaFactory metalFactory = new MetalPosudaFactory();
+            IPosudaFactory glassFactory = new GlassPosudaFactory();
+
+            Tuple<IList<IKnife>, IList<ISpoon>> metalPosuda = Creator(metalFactory);
+            Tuple<IList<IKnife>, IList<ISpoon>> glassPosuda = Creator(glassFactory);
+        }
+
+        static Tuple<IList<IKnife>, IList<ISpoon>> Creator(IPosudaFactory factory)
+        {
+            IList<IKnife> knifeList = new List<IKnife>();
+            IList<ISpoon> spoonList = new List<ISpoon>();
+
+            Tuple<IList<IKnife>, IList<ISpoon>> tuple = new Tuple<IList<IKnife>, IList<ISpoon>>(knifeList, spoonList);
+
+            knifeList.Add(factory.CreateKnife());
+            knifeList.Add(factory.CreateKnife());
+
+            spoonList.Add(factory.CreateSpoon());
+            spoonList.Add(factory.CreateSpoon());
+
+            return tuple;
+        }
+
+    }
+}
---
+++ ConcreteFactory/ConcreteFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactory;
+
+namespace ConcreteFactory
+{
+    public class GlassPosudaFactory : IPosudaFactory
+    {
+        public IKnife CreateKnife()
+        {
+            return new GlassKnife();
+        }
+
+        public ISpoon CreateSpoon()
+        {
+            return new GlassSpoon();
+        }
+    }
+    public class MetalPosudaFactory : IPosudaFactory
+    {
+        public IKnife CreateKnife()
+        {
+            return new MetalKnife();
+        }
+
+        public ISpoon CreateSpoon()
+        {
+            return new MetalSpoon();
+        }
+    }
+    class GlassKnife : IKnife
+    {
+    }
+    class GlassSpoon : ISpoon
+    {
+    }
+    class MetalKnife : IKnife
+    {
+    }
+    class MetalSpoon : ISpoon
+    {
+    }
+
+}
